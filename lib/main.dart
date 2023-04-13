ccbe8c768875c3028d8fc958bff854fb9aa56d80
Muhammad Ihsan Rahman <mihsanrahman19@gmail.com>
2023-04-13 12:55:15 +0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:telkomsel_app/screens/bottom_nav_bar.dart';
+import 'package:telkomsel_app/themes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      title: 'Telkomsel App Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: redColor,
+        ),
+      ),
+      home: BottomNavBar(),
+    );
+  }
+}
